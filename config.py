91fd59502070fab58ef46f7a58f2043129a60096
Notkartik6 <kartiksahane285@gmail.com>
2024-06-01 15:48:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/84819fc115cb0eff32b2b.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/6239dfec5c67ff47667b6.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/6239dfec5c67ff47667b6.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tbcbotschat")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tbc_bots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/makimamusic_bot00")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/makimamusic_bot00")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5090817443").split()))
 
